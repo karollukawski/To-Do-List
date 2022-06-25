6fd30425e1ffd142af146ba0bbf44163aadedbfb
karollukawski <83611706+karollukawski@users.noreply.github.com>
2022-06-25 10:25:58 +0200

    Basic model

---
+++ to-do_list.py
@@ -0,0 +1,19 @@
+while True:
+    print ()
+    print ("0. Show all tasks")
+    print ("1. Add a task")
+    print ("2. Delete a task")
+    print ("3. Exit")
+
+    choice = int(input ("Choose option:"))
+
+    if choice == 0:
+        print()
+    if choice == 1:
+        print()
+    if choice == 2:
+        print()
+    if choice == 3: 
+        break
+    else:
+        break
