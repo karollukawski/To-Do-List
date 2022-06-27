59318e14f2c3b87b361d4b22e5d619a53c1ddd7b
karollukawski <83611706+karollukawski@users.noreply.github.com>
2022-06-27 10:04:29 +0200

    Update v2

--- to-do_list.py
+++ to-do_list.py
@@ -13,7 +13,8 @@ while True:
     print ("0. Show all tasks")
     print ("1. Add a task")
     print ("2. Delete a task")
-    print ("3. Exit")
+    print ("3. Save changes in the file")
+    print ("4. Exit")
 
     choice = int(input ("Choose option:"))
 
@@ -24,6 +25,8 @@ while True:
     if choice == 2:
         delete_task()
     if choice == 3: 
+        print()
+    if choice == 4:
         break
     else:
         break
